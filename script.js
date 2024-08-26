4088e3828e374986ad7426607b640475204c68eb
Sashika_Piyumal <sasika.1776@gmail.com>
2024-08-26 13:40:02 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+function mouseoveron(element){
+    element.style.color='black';
+    element.style.fontSize ='18px';
+    element.style.fontWeight ='bold'
+
+}
+function mouseoverout(element){
+    element.style.color='#020744'
+    element.style.fontSize ='15px'
+    element.style.fontWeight = 'none'
+}
